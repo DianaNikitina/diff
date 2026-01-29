907d80779578444e3108281048217df0aa213969
DianaNikitina <110534493+DianaNikitina@users.noreply.github.com>
2026-01-29 22:58:04 +0300

    Add files via upload

--- differentiator/generatelatex.cpp
+++ differentiator/generatelatex.cpp
@@ -42,29 +42,6 @@ void generate_latex_document(trnode_t* original, trnode_t* derivative, const cha
     fprintf(f, "\n");
     fprintf(f, "\\end{equation*}\n");
     
-    /*fprintf(f, "\\section{Упрощенная форма (если применимо)}\n");
-    fprintf(f, "\\begin{equation*}\n");
-    fprintf(f, "f'(x) = ");
-    tree_to_latex(derivative, f); 
-    fprintf(f, "\n");
-    fprintf(f, "\\end{equation*}\n");
-    
-    fprintf(f, "\\section{Проверка}\n");
-    fprintf(f, "Проверим производную на некоторых значениях $x$:\n");
-    fprintf(f, "\\begin{itemize}\n");
-    fprintf(f, "    \\item При $x = 0$: $f'(0) = ...$\n");
-    fprintf(f, "    \\item При $x = 1$: $f'(1) = ...$\n");
-    fprintf(f, "    \\item При $x = \\pi$: $f'(\\pi) = ...$\n");
-    fprintf(f, "\\end{itemize}\n");
-    
-    fprintf(f, "\\section{Используемые правила дифференцирования}\n");
-    fprintf(f, "\\begin{enumerate}\n");
-    fprintf(f, "    \\item Правило суммы: $(u + v)' = u' + v'$\n");
-    fprintf(f, "    \\item Правило произведения: $(u \\cdot v)' = u' \\cdot v + u \\cdot v'$\n");
-    fprintf(f, "    \\item Правило частного: $\\left(\\frac{u}{v}\\right)' = \\frac{u' \\cdot v - u \\cdot v'}{v^2}$\n");
-    fprintf(f, "    \\item Цепное правило: $(f(g(x)))' = f'(g(x)) \\cdot g'(x)$\n");
-    fprintf(f, "\\end{enumerate}\n");*/
-    
     fprintf(f, "\\end{document}\n");
     
     fclose(f);
